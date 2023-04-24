135d3abfeda85ca837cf7944dea41ed9ccaa9b05
amedduman <amedduman@protonmail.com>
2023-04-25 02:50:09 +0300

    Build the project

--- Assets/_Game/Scripts/Player/States/PlayerWallJumpState.cs
+++ Assets/_Game/Scripts/Player/States/PlayerWallJumpState.cs
@@ -17,7 +17,7 @@ public class PlayerWallJumpState : State<Player>
         else
         {
             Owner.PlayAnimation("jump_start");
-            Owner.Rb.velocity = new Vector2(Owner.Rb.velocity.x, 16);
+            Owner.Rb.velocity = new Vector2(Owner.Rb.velocity.x, 20);
         }
     }
 
